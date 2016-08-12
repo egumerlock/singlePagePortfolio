25bf1a8f23cd0bd7cfc81181a5d57712398e9bde
Eric Gumerlock <eric.gumerlock@gmail.com>
2016-08-12 13:50:31 -0700

    Remove preloader, add detect mobile browser javascript

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -78,7 +78,7 @@ $('#contact-me-button').hover(function() {
 
 window.onload = function onLoad() {
     $('.button-wrapper').hide();
-    $('#preloader').delay(400).fadeOut("slow");
+
     $(window).scroll(function(){
         checkY1();
         checkX1();
